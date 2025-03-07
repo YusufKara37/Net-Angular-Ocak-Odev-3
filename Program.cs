07f776003fa72e3249047b5e609f4b6cfe89205c
Yusuf ismail Kara <yusufismailkara074@gmail.com>
2025-03-07 16:32:41 +0300

    dasdsa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+#region 1️-)Koşullu ifadeler (if-else) ne işe yarar? Gerçek hayattan bir örnek verin.
+    //85 üstü bir ortalaman varsa takdir alırsın(if)
+    //yoksa alamazsın(elese)
+#endregion
+
+#region 2️-)Kullanıcıdan bir sayı alıp tek mi çift mi olduğunu bulan bir C# programı yazın.
+    Console.WriteLine("lütfen bir sayı giriniz");
+    int sayi=Convert.ToInt32(Console.ReadLine());
+     if(sayi%2==0)
+     {
+        Console.WriteLine("girdiğiiz sayı çift");
+     }
+     else
+     {
+        Console.WriteLine("girdiğiniz sayı tek");
+     }
+#endregion
+
+#region 3️-)Girilen bir sayının pozitif, negatif veya sıfır olup olmadığını belirleyen bir program yazın.
+
+    int sayi2=Convert.ToInt32(Console.ReadLine());
+
+    if(sayi2>0)
+    {
+        Console.WriteLine("pozitif");
+    }
+    else
+    {
+        Console.WriteLine ("negatif");
+    }
+    
+#endregion
